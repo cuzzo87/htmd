8b6d49683ed6b45568bafd3701401216e11b27b3
cuzzo87 <cuzzolin.alberto@gmail.com>
2018-04-13 16:21:01 +0200

    testing quaternon

--- htmd/molecule/makemovie.py
+++ htmd/molecule/makemovie.py
@@ -99,6 +99,75 @@ class MovieMaker:
         end_scene = self.scenes[endSceneId]
         end_rotMat = end_scene.rotate_matrix
 
+    def makeTransaction(self, startSceneId, endSceneId):
+        self.retrieveScene(startSceneId)
+
+        beg_scene = self.scenes[startSceneId]
+        end_scene = self.scenes[endSceneId]
+
+        beg_eul = matrixToEuler(beg_scene.rotate_matrix)
+        end_eul = matrixToEuler(end_scene.rotate_matrix)
+
+        diff_eul = end_eul - beg_eul
+
+        end_eul = _bestEuler(diff_eul, end_eul)
+
+        diff_eul = end_eul - beg_eul
+
+        diff_center = end_scene.center_matrix - beg_scene.center_matrix
+        diff_scale = end_scene.scale_matrix - beg_scene.scale_matrix
+        diff_global = end_scene.global_matrix - beg_scene.global_matrix
+
+        _diffScene =  Scene()
+        _diffScene.rotate_matrix = diff_eul
+        _diffScene.center_matrix = diff_center
+        _diffScene.scale_matrix = diff_scale
+        _diffScene.global_matrix = diff_global
+
+        beg_rotmat_quat = matrixToQuaternion(beg_scene.rotate_matrix)
+        end_rotmat_quat = matrixToQuaternion(end_scene.rotate_matrix)
+
+        beg_centmat_quat = matrixToQuaternion(beg_scene.center_matrix)
+        end_centmat_quat = matrixToQuaternion(end_scene.center_matrix)
+
+        beg_scalemat_quat = matrixToQuaternion(beg_scene.scale_matrix)
+        end_scalemat_quat = matrixToQuaternion(end_scene.scale_matrix)
+
+        beg_globalmat_quat = matrixToQuaternion(beg_scene.global_matrix)
+        end_globalmat_quat = matrixToQuaternion(end_scene.global_matrix)
+
+        self.saveScene()
+        now_scene = self.scenes[-1]
+
+        now_centmat = now_scene.center_matrix
+        now_scalemat = now_scene.scale_matrix
+        now_globmat = now_scene.global_matrix
+        import time
+        for i in range(50):
+            print(i)
+            step = (i+1)/50
+            qarc = quatarc(beg_rotmat_quat, end_rotmat_quat, step)
+            now_rotmat = quaternionToMatrix(qarc)
+            #now_centmat = quaternionToMatrix(quatarc(beg_centmat_quat, end_centmat_quat, step))
+            #now_scalemat = quaternionToMatrix(quatarc(beg_scalemat_quat, end_scalemat_quat, step))
+            #now_globmat = quaternionToMatrix(quatarc(beg_globalmat_quat, end_globalmat_quat, step))
+            # now_centmat = np.add(now_centmat, _diffScene.center_matrix * step)
+            # now_scalemat = np.add(now_scalemat, _diffScene.scale_matrix * step)
+            # now_globmat = np.add(now_globmat, _diffScene.global_matrix * step)
+            rot_mat = str(now_rotmat.tolist())
+            rot_mat = rot_mat.replace('[', '{').replace(']', '}').replace(',', '')
+            center_mat = str(now_centmat.tolist())
+            center_mat = center_mat.replace('[', '{').replace(']', '}').replace(',', '')
+            scale_mat = str(now_scalemat.tolist())
+            scale_mat = scale_mat.replace('[', '{').replace(']', '}').replace(',', '')
+            glob_mat = str(now_globmat.tolist())
+            glob_mat = glob_mat.replace('[', '{').replace(']', '}').replace(',', '')
+            self.viewer.send('molinfo top set rotate_matrix {{{}}}'.format(rot_mat))
+            #self.viewer.send('molinfo top set {{rotate_matrix center_matrix scale_matrix global_matrix}} '
+            #               '{{{} {} {} {}}}'.format(rot_mat, center_mat, scale_mat, glob_mat))
+            time.sleep(0.2)
+
+
     def _matrixToVMD(self, scene, matrixtype):
 
         viewer = self.viewer
@@ -209,7 +278,8 @@ def matrixToQuaternion(matrix):
         z = (m12 - m21) * s
 
     elif m11 > m22 and m11 > m33:
-        r = 1.0 - t44as33 + 2 * m11
+        #r = 1.0 - t44as33 + 2 * m11
+        r = 1.0 +  m11 + -m22 -m33 #+  m11
         s = 0.5 / sqrt(r)
         w = s * r
         x = (m12 + m21) * s
@@ -217,7 +287,8 @@ def matrixToQuaternion(matrix):
         z = (m23 - m32) * s
 
     elif m22 > m33:
-        r = 1.0 - t44as33 + 2 * m22
+        #r = 1.0 - t44as33 + 2 * m22
+        r = 1.0 + -m11 + m22 -m33
         s = 0.5 / sqrt(r)
         w = s * r
         x = (m12 + m21) * s
@@ -225,7 +296,8 @@ def matrixToQuaternion(matrix):
         z = (m31 - m13) * s
 
     else:
-        r = 1.0 - t44as33 + 2 * m33
+        #r = 1.0 - t44as33  + 2 * m33
+        r = 1.0 + -m11 -m22 + m33
         s = 0.5 / sqrt(r)
         w = s * r
         x = (m13 + m31) * s
@@ -235,19 +307,34 @@ def matrixToQuaternion(matrix):
     return np.array([w,x,y,z])
 
 def quatarc(quat1, quat2, step):
-    from math import sqrt, acos, sin
-
+    from math import sqrt, acos, sin, degrees, atan, atan2
+    print(quat1, quat2)
     qdot = np.dot(quat1, quat2)
+    print(qdot)
 
     if qdot > 0.9999:
         return quat2
     elif qdot < 0:
         quat1 = quat1 * -1
 
-    theta = acos(np.dot(quat1, quat2)/sqrt(np.dot(quat1, quat1) * np.dot(quat2, quat2)))
+    #theta = acos(np.dot(quat1, quat2)/sqrt(np.dot(quat1, quat1) * np.dot(quat2, quat2)))
+    #theta = acos(np.dot(quat1, quat2))
+    quat2Conj = _invQuaternion(quat2)
+   # print("shape: ", quat1.shape, quat2Conj.shape)
+    #print(">>>>> ", np.cross(quat1, quat2Conj))
+    # {\displaystyle
+    # {} + (a_{1}d_{2}+b_{1}c_{2}-c_{1}b_{2}+d_{1}a_{2})
+    # k.} {} + (a_{1}d_{2}+b_{1}c_{2}-c_{1}b_{2}+d_{1}a_{2})
+    # k.
 
-    quat = np.add( sin(theta * (1-step))/sin(theta) * quat1, sin(theta * step)/sin(theta) * quat2 )
 
+    theta = atan2((quat1 * quat2Conj)[0])
+    print("a: ", degrees(theta), step)
+
+#    quat = np.add( sin(theta * (1-step))/sin(theta) * quat1, sin(theta * step)/sin(theta) * quat2 )
+    quat = np.add(sin(theta * (1 - step)) * quat1, sin(theta * step) * quat2)  / sin(theta)
+    print(quat)
+    print()
     return quat
 
 def quaternionToMatrix(quat):
@@ -274,3 +361,8 @@ def _bestEuler(diff_euler, euler):
         elif diff_euler[i] < -pi:
             new_euler[i] = euler[i] + 2 * pi
     return np.array(new_euler)
+
+def _invQuaternion(quat):
+    quatInv = quat * [1,-1,-1,-1]
+
+    return quatInv
